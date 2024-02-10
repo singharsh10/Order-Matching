2ed41c8037d4610451b1943b10678ae858990e40
Harsh Prasad Singh <gmcode10jp@gmail.com>
2024-02-10 14:43:53 +0530

    modified takeOrder()

--- main.cpp
+++ main.cpp
@@ -1,80 +1,114 @@
 #include <map>
-#include "order.h"
+#include "order_book.h"
 
 using namespace std::chrono;
+using namespace ordermatching;
 
-std::vector<Symbol> sell_order_symbols;
-std::map<std::string, int> sell_sym_at_idx;
-std::vector<Symbol> buy_order_symbols;
-std::map<std::string, int> buy_sym_at_idx;
 
-// call this till Buy queue is not empty
 void takeOrders() {
 	
+	std::map<std::string, OrderBook> buy_side_symbols;
+	std::map<std::string, OrderBook> sell_side_symbols;
+
 	while (true) {
 		
 		std::string order_type;
-		float price;
-		uint64_t qty;
-		uint64_t id;
+		Price price;
+		Quantity qty;
+		Id id;
 		std::string symbol;
 		time_point<system_clock> curr_time = system_clock::now();
 
 		std::cin >> order_type >> price >> qty >> id >> symbol;
+		
+		/*
+			TODO
+			in case qty <= 0 or price < 0 throw error
+		 	don't construct order if it's a modify order
+		*/
 
-		Order received_order = Order(price, qty, id, curr_time);
+		Order received_order = Order(order_type, price, qty, id, curr_time);
 
-		auto sell_sym_idx = sell_sym_at_idx.find(symbol);
-		auto buy_sym_idx = buy_sym_at_idx.find(symbol);
+		if (order_type == "BUY") {
 
-		if (order_type == "SELL") {
+			auto symbol_orderbook = sell_side_symbols.find(symbol);
+			auto have_symbol = buy_side_symbols.find(symbol);
 
-			if (buy_sym_idx != buy_sym_at_idx.end()) {
+			if (symbol_orderbook == sell_side_symbols.end()) {
 
-				buy_order_symbols[(*buy_sym_idx).second].executeSellOrder(received_order);
+				if (have_symbol == buy_side_symbols.end())
+					have_symbol = buy_side_symbols.emplace(symbol, OrderBook(symbol)).first;
+				
+				/* can't use this buy_side_symbols[symbol].addOrder(received_order). It won't 
+				 * compile as compiler needs default constructor even if key is present
+				 * It does so to make sure in case the key is not present then a default object 
+				 * can be constructed. Compiler does not know that this key will always exists. 
+				 * Hence it complains.
+				 */
+
+				(*have_symbol).second.addOrder(received_order);
 			}
+			else {
 
-			if (received_order.getQuantity() > 0) {
+				(*symbol_orderbook).second.matchOrder(received_order);
 
-				if (sell_sym_idx == sell_sym_at_idx.end()) {
+				if (received_order.getQuantity() > 0) {
 
-					sell_sym_at_idx[symbol] = sell_order_symbols.size();
-					sell_order_symbols.push_back(Symbol(symbol));
-					sell_order_symbols.back().addSellOrder(received_order);
+					if (have_symbol == buy_side_symbols.end())
+						have_symbol = buy_side_symbols.emplace(symbol, OrderBook(symbol)).first;
+
+					(*have_symbol).second.addOrder(received_order);
 				}
 				else {
 
-					sell_order_symbols[(*sell_sym_idx).second].addSellOrder(received_order);
+					std::cout << "BUY order with order id: " << received_order.getOrderId() << " traded" << std::endl;
+					Order::id_to_order.erase(id);
 				}
-			}
-			else {
-				/* cout order id and order traded at time */
-				std::cout << "Order traded with Order ID: " << id << std::endl;
 			}
 		}
-		else {
-			
-			if (sell_sym_idx != sell_sym_at_idx.end()) {
+		else if (order_type == "SELL") {
 
-				sell_order_symbols[(*sell_sym_idx).second].executeBuyOrder(received_order);
+			auto symbol_orderbook = buy_side_symbols.find(symbol);
+			auto have_symbol = sell_side_symbols.find(symbol);
+
+			if (symbol_orderbook == buy_side_symbols.end()) {
+
+				if (have_symbol == sell_side_symbols.end())
+					have_symbol = sell_side_symbols.emplace(symbol, OrderBook(symbol)).first;
+
+				(*have_symbol).second.addOrder(received_order);
 			}
+			else {
 
-			if (received_order.getQuantity() > 0) {
-				
-				if (buy_sym_idx == buy_sym_at_idx.end()) {
+				(*symbol_orderbook).second.matchOrder(received_order);
 
-					buy_sym_at_idx[symbol] = buy_order_symbols.size();
-					buy_order_symbols.push_back(Symbol(symbol));
-					buy_order_symbols.back().addBuyOrder(received_order);
+				if (received_order.getQuantity() > 0) {
+
+					if (have_symbol == sell_side_symbols.end())
+						have_symbol = sell_side_symbols.emplace(symbol, OrderBook(symbol)).first;
+
+					(*have_symbol).second.addOrder(received_order);
 				}
 				else {
 
-					buy_order_symbols[(*buy_sym_idx).second].addBuyOrder(received_order);
+					std::cout << "SELL order with order id: " << received_order.getOrderId() << " traded" << std::endl;
+					Order::id_to_order.erase(id);
 				}
 			}
+		}
+		else {	// modify order
+
+			Order original_order = Order((*Order::id_to_order.find(id)).second);
+			
+			if (original_order.getSide() == BUY) {
+						
+				auto symbol_orderbook = buy_side_symbols.find(symbol);
+				(*symbol_orderbook).second.modifyOrder(original_order, qty, price);
+			}
 			else {
-				/* cout order id and order traded at time */
-				std::cout << "Order traded with Order ID: " << id << std::endl;
+
+				auto symbol_orderbook = sell_side_symbols.find(symbol);
+				(*symbol_orderbook).second.modifyOrder(original_order, qty, price);
 			}
 		}
 	}
